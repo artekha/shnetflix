5005d8c926c9feb3fcb79df45c57e118603726c1
artekha <andrewartekha@gmail.com>
2019-09-22 17:13:06 +0300

    Create logo and search field

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SearchField from '../SearchField';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="app__title">Shnetflix</h1>
+      <SearchField></SearchField>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SearchField/SearchField.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input } from 'reactstrap';
+
+const SearchField = () => (
+  <Input className="search-field" placeholder="Start typing..."></Input>
+);
+
+export default SearchField;
